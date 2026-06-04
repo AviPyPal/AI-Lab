826245a8e334886898e102ae35fd73351a834f04
Avinash Pal <avinash.pal@wabtec.com>
2026-06-03 19:42:47 -0500

    Introduce modular code structure with utils module

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+from utils.printer import print_message
+
+
 def main():
     print("AI Lab is set up successfully.")
     print("You are now building like a professional AI engineer.")
+
+    print_message()
 
 
 if __name__ == "__main__":
---
+++ src/utils/printer.py
@@ -0,0 +1,4 @@
+# src/utils/printer.py
+
+def print_message():
+    print("This is coming from a reusable module.")
